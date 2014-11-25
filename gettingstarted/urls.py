6bb3aa87a0b67600158290fb331e10330a2eda07
David Burke <dmburke11@yahoo.com>
2014-11-25 12:22:41 -0800

    initial load

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -10,8 +10,12 @@ urlpatterns = patterns('',
     # url(r'^$', 'gettingstarted.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', hello.views.index, name='index'),
+    url(r'^heroku/$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'hello.views.Home', name='savemoney-home'),
+    url(r'^save/$', 'hello.views.SaveMoneyForm', name='save-submit-form'),
+    url(r'^savehistory/$', 'hello.views.ShowHistory', name='show-history-form'),
+]
 
 )
--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django import template
+from django.core.urlresolvers import reverse
 
 from .models import Greeting
 
@@ -16,4 +18,41 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
+
+
+
+def Home(request):
+	x = 0
+	y = 0
+
+	try:
+		x = Decimal(request.GET.get('x', 0))
+		y = Decimal(request.GET.get('y', 0))
+	except Exception, e:
+		return HttpResponse("Something went wrong: %s" % str(e) )
+
+	result = add_numbers(x,y)
+	context = {
+		'result': result,
+		'user': "david burke" # request.user.first_name
+	}
+	return render(request, 'save_money_input.html', context)
+
+
+def SaveMoneyForm(request):
+	print request.POST
+	text_box = request.POST['txt_value']
+	
+
+	return HttpResponseRedirect(reverse('savemoney-home'))
+
+def ShowHistory(request):
+	return HttpResponse("Savings History:")
+
+
+def add_numbers(a,b):
+	return a+b
+
+
+
 
