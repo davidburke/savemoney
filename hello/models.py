b58079808cfab9c8975da28793539125919b674c
David Burke <dmburke11@yahoo.com>
2014-11-25 20:02:32 -0800

    working w db

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+class Entry(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    reason = models.CharField(max_length=30)
+
--- hello/views.py
+++ hello/views.py
@@ -2,38 +2,39 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django import template
 from django.core.urlresolvers import reverse
+from decimal import Decimal
+from django.db.models import Count, Min, Sum, Avg
+from .models import Greeting, Entry
 
-from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+	return HttpResponse('Hello from Python!')
 
 
 def db(request):
+	greeting = Greeting()
+	greeting.save()
 
-    greeting = Greeting()
-    greeting.save()
+	greetings = Greeting.objects.all()
 
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
-
+	return render(request, 'db.html', {'greetings': greetings})
 
 
 def Home(request):
-	x = 0
-	y = 0
 
-	try:
-		x = Decimal(request.GET.get('x', 0))
-		y = Decimal(request.GET.get('y', 0))
-	except Exception, e:
-		return HttpResponse("Something went wrong: %s" % str(e) )
+	entries = Entry.objects.all().order_by('-date', 'amount')
+	total_entries = Entry.objects.aggregate(total=Sum('amount'), average=Avg('amount'))
 
-	result = add_numbers(x,y)
+
+	total = 0
+	for e in entries:
+		total += e.amount
+
 	context = {
-		'result': result,
+		'entries': entries,
+		'total': total,
+		'total_entries': total_entries,
 		'user': "david burke" # request.user.first_name
 	}
 	return render(request, 'save_money_input.html', context)
@@ -42,7 +43,11 @@ def Home(request):
 def SaveMoneyForm(request):
 	print request.POST
 	text_box = request.POST['txt_value']
-	
+	reason = 'None Provided'
+	newEntry = Entry()
+	newEntry.amount = Decimal(text_box)
+	newEntry.reason = reason
+	newEntry.save()
 
 	return HttpResponseRedirect(reverse('savemoney-home'))
 
@@ -50,8 +55,9 @@ def ShowHistory(request):
 	return HttpResponse("Savings History:")
 
 
-def add_numbers(a,b):
+def add_numbers(a, b):
 	return a+b
+
 
 
 
